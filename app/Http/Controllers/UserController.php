7e4853d9f441966dbaac9be63c41d4e00036667f
Sincan Maulana <sincanmaulanaa@gmail.com>
2022-11-09 23:33:40 +0800

    create user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,53 @@ class UserController extends Controller
 {
     public function tampil()
     {
-        $dataUser = User::all();
-        echo $dataUser;
+        $data_user = User::all();
+        return view("admin.postingan")->with("data_user", $data_user);
+    }
+
+    public function formInput()
+    {
+        return view("admin.form");
+    }
+
+    public function simpan(Request $request)
+    {
+        $user = new User();
+        $user->name = $request->get("name");
+        $user->username = $request->get("username");
+        $user->email = $request->get("email");
+        $user->password = $request->get("password");
+        $user->save();
+
+        return redirect(route("postingan"));
+    }
+
+    public function formEdit($id)
+    {
+        return view("admin.update")->with("id", $id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = User::find($id);
+        $user->name = $request->get("name");
+        $user->username = $request->get("username");
+        $user->email = $request->get("email");
+        $user->password = $request->get("password");
+        $user->save();
+
+        return redirect(route("postingan"));
+    }
+
+    public function delete($id)
+    {
+        User::destroy($id);
+        return redirect(route("postingan"));
+    }
+
+    public function detail($id)
+    {
+        $data_user = User::find($id); // ambil data pada tabel user berdasarkan id
+        return view("admin.detail")->with("data_user", $data_user); // tampilkan menggunakan view
     }
 }
